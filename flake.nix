fcb25e048bff2b9fa0a70b85d4d07951aa6e1022
EOWNERDEAD <eownerdead@disroot.org>
2024-03-01 02:12:14 +0900

    Package

--- flake.nix
+++ flake.nix
@@ -6,9 +6,27 @@
 
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) lib;
       in {
         formatter = pkgs.nixfmt;
+
+        packages.eglot-semantic-tokens = with pkgs.emacs.pkgs;
+          trivialBuild {
+            pname = "eglot-semantic-tokens";
+            version = "master";
+
+            src = ./eglot-semantic-tokens.el;
+
+            meta = with lib; {
+              description = "eglot semantic tokens support";
+              homepage =
+                "https://codeberg.org/eownerdead/eglot-semantic-tokens";
+              license = licenses.gpl3Only;
+              platforms = platforms.all;
+            };
+          };
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [ editorconfig-checker nixfmt ];
